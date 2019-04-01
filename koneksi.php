ee6d2589e75b4e8467796404bd54b2b61e21f662
MuhammadFardanIqbaludin <48473106+MuhammadFardanIqbaludin@users.noreply.github.com>
2019-04-01 11:42:02 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$koneksi = mysqli_connect("localhost","tugas3","","id7899367_tugas3");
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+ 
+?>
